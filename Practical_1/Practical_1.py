32842aa37870e7632f0b98097ffd789add9b44e6
Leejames2305 <leejames2305@gmail.com>
2026-02-09 22:18:48 +0800

    feat: Parts of Practical 1

---
+++ Practical_1/Practical_1.py
@@ -0,0 +1,12 @@
+## Practical 1 - Basics
+
+# 1.1 Comment
+x = 1 # This is a comment
+
+# 1.2 Data Types
+print(type(True))
+print(type(5))
+print(type(3.14))
+print(type("Hello"))
+
+input('Press Enter to continue...')
